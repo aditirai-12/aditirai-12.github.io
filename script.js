2211affef17e63bcf1ae024bd4478881c1b942fb
Aditi  Rai <aditirai@MacBook-Pro.local>
2023-05-25 22:07:59 -0700

    update navbar react display

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ ReactDOM.render(
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" href="#mission-section">About Me</a>
+            <a class="nav-link" href="index.html">About Me</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="experience.html">Experience</a>
